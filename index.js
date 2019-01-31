445671483c65472cb1acda0d9d3f14843a2db6c4
valtteri459 <valtteri.valonen@gmail.com>
2019-01-31 21:05:42 +0200

    asdaasd

--- index.js
+++ index.js
@@ -53,18 +53,18 @@ var pwm = new servoDriver(options, (err) => {
 	 });*/
 	 io.on('connection', socket => {
 		 console.log('user connected!')
-		 socket.on('disconnect', (reason) => {
+		 socket.on('servo', data => {
+			console.log(data)
+			var servo = data.channel
+			var val = data.val > 150 ? data.val < 600 ? data.val : 600 : 150
+			pwm.setPulseRange(servo, 0, val, (err) => {
+				if(err) {
+					console.log(err)
+				}
+			})
+		})
+		socket.on('disconnect', (reason) => {
 			 console.log('user disconnected', reason)
-		 })
-	 })
-	 io.on('servo', data => {
-		 console.log(data)
-		 var servo = data.channel
-		 var val = data.val > 150 ? data.val < 600 ? data.val : 600 : 150
-		 pwm.setPulseRange(servo, 0, val, (err) => {
-			 if(err) {
-				 console.log(err)
-			 }
 		 })
 	 })
 	app.get('/', (req, res) => {
