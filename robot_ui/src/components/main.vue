52eba5074b2833b4d15c340654cca9c7e99a77ea
valtteri459 <valtteri.valonen@gmail.com>
2019-01-31 21:10:40 +0200

    v1 done

--- robot_ui/src/components/main.vue
+++ robot_ui/src/components/main.vue
@@ -7,7 +7,9 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
-        <v-btn color="primary" @click="$socket.emit('servo', {channel: 0, val: 160})">uh ah</v-btn>
+        <input type="number" v-model.number="channel" placeholder="kanava"/>
+        <input type="number" v-model.number="val" placeholder="arvo"/>
+        <v-btn color="primary" @click="sendNow()">uh ah</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
@@ -16,8 +18,22 @@
 <script>
 export default {
   data: () => ({
-    message: 'helloWorld'
+    message: 'helloWorld',
+    channel: 0,
+    val: 400
   }),
+  methods: {
+    /* servo 0
+      coin hopper: 530
+      camera: 320
+      drop: 150
+      servo 1
+      leftmost: 465
+      rightmost: 240 */
+    sendNow () {
+      this.$socket.emit('servo', { channel: this.channel, val: this.val })
+    }
+  },
   sockets: {
     connect: () => {
       console.log('socket connected!')
