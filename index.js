b092bdaef51c8e0bca28fad987016ecc0205519d
valtteri459 <valtteri.valonen@gmail.com>
2019-03-11 22:12:14 +0200

    asdasdasd

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const io = require('socket.io')(http)
 const jpeg = require('jpeg-js')
 const fs = require('fs')
 const tf = require('@tensorflow/tfjs-node')
+let model = null
 tf.loadLayersModel('file://./net/model.json').then(mdl => {
-	const model = mdl
+	model = mdl
 }).catch(e => console.log(e))
 /* 
 servo 0
